2d79533baa09c170c56d47b0a2138276dd143e7f
David Sardari <d@duxsco.de>
2021-10-02 17:00:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# (WIP) GnuPG Web of Trust (WIP)
+
+## Background
+
+GnuPG's Web of Trust approach is long dead due to:
+
+- [Certificate spamming attacks](https://gist.github.com/rjhansen/67ab921ffb4084c865b3618d6955275f)
+- disinterest in campaigns such as:
+  - [CAcert](http://www.cacert.org): [Signing key](http://www.cacert.org/index.php?id=3) using old `dsa1024` and `elg2048`
+  - [DFN](https://web.archive.org/web/20070613205827/https://www.pki.dfn.de/content/index.php?id=pgp): Offline sometime after 2007 and never really for public use (AFAIK)
+  - [Heise crypto compaign](https://www.heise.de/security/dienste/Krypto-Kampagne-2111.html): Still publishing to dead SKS keyservers ([see last bullet point](https://www.heise.de/security/dienste/Wie-kann-ich-mitmachen-474837.html)) and [no consideration for ECC keys](https://www.heise.de/security/dienste/Zertifizierungsantrag-474471.html)
+- and finally privacy concerns due to [GDPR](https://en.wikipedia.org/wiki/General_Data_Protection_Regulation):
+
+![sks keyserver notification](assets/sks_keyserver_notification.png)
+
+## Direct trust approach
+
+I created the repo [gpg-config-and-scripts](https://github.com/duxco/gpg-config-and-scripts) with configuration files that enforce setting key validity directly without any dependence on Web of Trust. Furthermore, scripts are provided to:
+
+  - check for public key delivery options (e.g. DANE, WKD) and pull keys from a suitable source ([pull.sh](https://github.com/duxco/gpg-config-and-scripts/blob/main/bin/pull.sh))
+  - display man pages while highlighting defaults (in yellow) and options that have been set in the configuration file (in red) ([man.sh](https://github.com/duxco/gpg-config-and-scripts/blob/main/bin/man.sh))
+  - manage the public key store ([pubkey.sh](https://github.com/duxco/gpg-config-and-scripts/blob/main/bin/pubkey.sh))
+
+## Signing GnuPG public keys with S/MIME
+
+But, I still had the wish to provide some verification mechanism while publishing my GnuPG public key. Thus, I decided to build upon class 3 S/MIME certificates issued by [CAcert](http://www.cacert.org) to sign my GnuPG public keys.
+
+### S/MIME keypair creation
+
+To get started you first have to get a valid certificate issued:
+
+1. First, you have to create your private key for S/MIME and generate a CSR. Unfortunately, [CAcert](http://www.cacert.org) doesn't support ECC. Thus, I am using RSA3072, the next best option (IMHO).
+
+```bash
+( umask 0177 && openssl genrsa -aes256 -out smime.key 3072 )
+# We can leave the subject and SAN empty,
+# because they will be ignored by cacert.org.
+openssl req -new -sha256 -key smime.key -subj "/" -out smime.csr
+```
+
+2. Get your S/MIME certificate issued:
+
+![certificate request](assets/certificate_request.png)
+
+3. Download your certificate:
+
+![certificate download](assets/certificate_download.png)
+
+4. Create a PKCS 12 file:
+
+```bash
+openssl pkcs12 -export -in smime.crt -inkey smime.key -out smime.pkcs12
+```
+
+### GnuPG keypair signing
+
+I refrain from using GnuPG's Web of Trust approach. Thus, I am doing a minimal export of my public key excluding all signatures except the most recent self-signature on each user ID.
+
+1. Export your GnuPG public key:
+
+```bash
+gpg --export-options export-minimal --export --armor "YOUR KEY ID" > pubkey.asc
+```
+
+2. Sign your GnuPG public key:
+
+```bash
+openssl pkcs12 -in smime.pkcs12 -nodes | openssl smime -sign -signer - -in pubkey.asc -out pubkey.asc.msg
+```
+
+`pubkey.asc.msg` is the file you can publish.
+
+## Verification of S/MIME signature and GnuPG public key import
+
+### ⚠ Disclaimer ⚠
+
+`s2g.sh` is still WIP. Currently, I cannot fully test `s2g.sh`:
+
+- I am currently trying to recover my old [CAcert](http://www.cacert.org) account using [Password Recovery with Assurance](https://wiki.cacert.org/FAQ/LostPasswordOrAccount#Password_Recovery_with_Assurance) in order to be able to get a class 3 S/MIME certificate issued again.
+- https://crl.cacert.org delivers an expired intermediate certificate ([see](https://www.ssllabs.com/ssltest/analyze.html?d=crl.cacert.org&latest)
+- OCSP responses are signed by an expired certificate:
+
+```bash
+openssl ocsp -CAfile cacert.org_class1.crt -issuer cacert.org_class1.crt -cert cacert.org_class1.crt -url http://ocsp.cacert.org -text | openssl x509 -dates -noout
+Response Verify Failure
+139665196160384:error:27069065:OCSP routines:OCSP_basic_verify:certificate verify error:crypto/ocsp/ocsp_vfy.c:92:Verify error:certificate has expired
+139665196160384:error:27069065:OCSP routines:OCSP_basic_verify:certificate verify error:crypto/ocsp/ocsp_vfy.c:92:Verify error:certificate has expired
+notBefore=Aug 25 14:12:48 2019 GMT
+notAfter=Aug 24 14:12:48 2021 GMT
+```
+
+The expected behaviour for class 1 certificates is like that shown for class 3:
+
+```bash
+echo | openssl s_client -CAfile cacert.org_class3.crt -servername www.cacert.org -connect www.cacert.org:443 2>/dev/null | openssl x509 | openssl ocsp -CAfile cacert.org_class1.crt -issuer cacert.org_class3.crt -cert - -url http://ocsp.cacert.org -text | openssl x509 -dates -noout
+Response verify OK
+notBefore=Aug 24 20:34:34 2021 GMT
+notAfter=Aug 24 20:34:34 2023 GMT
+```
+
+I already notified support of the problems with CRL and OCSP and awaiting a response.
+
+### `s2g.sh` - S/MIME signed GnuPG
+
+[CAcert](http://www.cacert.org) class 1 and class 3 root certificates have been integrated in `s2g.sh` (**S**/IME **s**igned **G**nuPG). To verify them print their fingerprint ([credits](https://kdecherf.com/blog/2015/04/10/show-the-certificate-chain-of-a-local-x509-file/)):
+
+```bash
+awk -F'\n' '
+        BEGIN {
+            ind = 1
+            showcert = "openssl x509 -fingerprint -noout -sha256 2>/dev/null"
+        }
+
+        /-----BEGIN CERTIFICATE-----/ {
+            printf "Class%2d: ", ind
+        }
+
+        {
+            printf $0"\n" | showcert
+        }
+
+        /-----END CERTIFICATE-----/ {
+            close(showcert)
+            ind ++
+            ind ++
+        }
+    ' s2g.sh
+```
+
+And, compare them with the hashes published by the CAcert ([HTTP](http://www.cacert.org/index.php?id=3) or [HTTPS](https://www.cacert.org/index.php?id=3)).
+
+To verify and import your communication partner's GnuPG public key do:
+
+```bash
+# Just verify
+bash s2g.sh pubkey.asc.msg
+
+# If verification success, you can import the traditional way
+gpg --import pubkey.asc.msg
+```
---
+++ s2g.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+
+set -eop pipefail
+
+CLASS1_ROOT_CRT="
+-----BEGIN CERTIFICATE-----
+MIIG7jCCBNagAwIBAgIBDzANBgkqhkiG9w0BAQsFADB5MRAwDgYDVQQKEwdSb290
+IENBMR4wHAYDVQQLExVodHRwOi8vd3d3LmNhY2VydC5vcmcxIjAgBgNVBAMTGUNB
+IENlcnQgU2lnbmluZyBBdXRob3JpdHkxITAfBgkqhkiG9w0BCQEWEnN1cHBvcnRA
+Y2FjZXJ0Lm9yZzAeFw0wMzAzMzAxMjI5NDlaFw0zMzAzMjkxMjI5NDlaMHkxEDAO
+BgNVBAoTB1Jvb3QgQ0ExHjAcBgNVBAsTFWh0dHA6Ly93d3cuY2FjZXJ0Lm9yZzEi
+MCAGA1UEAxMZQ0EgQ2VydCBTaWduaW5nIEF1dGhvcml0eTEhMB8GCSqGSIb3DQEJ
+ARYSc3VwcG9ydEBjYWNlcnQub3JnMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIIC
+CgKCAgEAziLA4kZ97DYoB1CW8qAzQIxL8TtmPzHlawI229Z89vGIj053NgVBlfkJ
+8BLPRoZzYLdufujAWGSuzbCtRRcMY/pnCujW0r8+55jE8Ez64AO7NV1sId6eINm6
+zWYyN3L69wj1x81YyY7nDl7qPv4coRQKFWyGhFtkZip6qUtTefWIonvuLwphK42y
+fk1WpRPs6tqSnqxEQR5YYGUFZvjARL3LlPdCfgv3ZWiYUQXw8wWRBB0bF4LsyFe7
+w2t6iPGwcswlWyCR7BYCEo8y6RcYSNDHBS4CMEK4JZwFaz+qOqfrU0j36NK2B5jc
+G8Y0f3/JHIJ6BVgrCFvzOKKrF11myZjXnhCLotLddJr3cQxyYN/Nb5gznZY0dj4k
+epKwDpUeb+agRThHqtdB7Uq3EvbXG4OKDy7YCbZZ16oE/9KTfWgu3YtLq1i6L43q
+laegw1SJpfvbi1EinbLDvhG+LJGGi5Z4rSDTii8aP8bQUWWHIbEZAWV/RRyH9XzQ
+QUxPKZgh/TMfdQwEUfoZd9vUFBzugcMd9Zi3aQaRIt0AUMyBMawSB3s42mhb5ivU
+fslfrejrckzzAeVLIL+aplfKkQABi6F1ITe1Yw1nPkZPcCBnzsXWWdsC4PDSy826
+YreQQejdIOQpvGQpQsgi3Hia/0PsmBsJUUtaWsJx8cTLc6nloQsCAwEAAaOCAX8w
+ggF7MB0GA1UdDgQWBBQWtTIb1Mfz4OaO873SsDrusjkY0TAPBgNVHRMBAf8EBTAD
+AQH/MDQGCWCGSAGG+EIBCAQnFiVodHRwOi8vd3d3LmNhY2VydC5vcmcvaW5kZXgu
+cGhwP2lkPTEwMFYGCWCGSAGG+EIBDQRJFkdUbyBnZXQgeW91ciBvd24gY2VydGlm
+aWNhdGUgZm9yIEZSRUUgaGVhZCBvdmVyIHRvIGh0dHA6Ly93d3cuY2FjZXJ0Lm9y
+ZzAxBgNVHR8EKjAoMCagJKAihiBodHRwOi8vY3JsLmNhY2VydC5vcmcvcmV2b2tl
+LmNybDAzBglghkgBhvhCAQQEJhYkVVJJOmh0dHA6Ly9jcmwuY2FjZXJ0Lm9yZy9y
+ZXZva2UuY3JsMDIGCCsGAQUFBwEBBCYwJDAiBggrBgEFBQcwAYYWaHR0cDovL29j
+c3AuY2FjZXJ0Lm9yZzAfBgNVHSMEGDAWgBQWtTIb1Mfz4OaO873SsDrusjkY0TAN
+BgkqhkiG9w0BAQsFAAOCAgEAR5zXs6IX01JTt7Rq3b+bNRUhbO9vGBMggczo7R0q
+Ih1kdhS6WzcrDoO6PkpuRg0L3qM7YQB6pw2V+ubzF7xl4C0HWltfzPTbzAHdJtja
+JQw7QaBlmAYpN2CLB6Jeg8q/1Xpgdw/+IP1GRwdg7xUpReUA482l4MH1kf0W0ad9
+4SuIfNWQHcdLApmno/SUh1bpZyeWrMnlhkGNDKMxCCQXQ360TwFHc8dfEAaq5ry6
+cZzm1oetrkSviE2qofxvv1VFiQ+9TX3/zkECCsUB/EjPM0lxFBmu9T5Ih+Eqns9i
+vmrEIQDv9tNyJHuLsDNqbUBal7OoiPZnXk9LH+qb+pLf1ofv5noy5vX2a5OKebHe
++0Ex/A7e+G/HuOjVNqhZ9j5Nispfq9zNyOHGWD8ofj8DHwB50L1Xh5H+EbIoga/h
+JCQnRtxWkHP699T1JpLFYwapgplivF4TFv4fqp0nHTKC1x9gGrIgvuYJl1txIKmx
+XdfJzgscMzqpabhtHOMXOiwQBpWzyJkofF/w55e0LttZDBkEsilV/vW0CJsPs3eN
+aQF+iMWscGOkgLFlWsAS3HwyiYLNJo26aqyWPaIdc8E4ck7Sk08WrFrHIK3EHr4n
+1FZwmLpFAvucKqgl0hr+2jypyh5puA3KksHF3CsUzjMUvzxMhykh9zrMxQAHLBVr
+Gwc=
+-----END CERTIFICATE-----
+"
+
+CLASS3_ROOT_CRT="
+-----BEGIN CERTIFICATE-----
+MIIGPTCCBCWgAwIBAgIDFOIoMA0GCSqGSIb3DQEBDQUAMHkxEDAOBgNVBAoTB1Jv
+b3QgQ0ExHjAcBgNVBAsTFWh0dHA6Ly93d3cuY2FjZXJ0Lm9yZzEiMCAGA1UEAxMZ
+Q0EgQ2VydCBTaWduaW5nIEF1dGhvcml0eTEhMB8GCSqGSIb3DQEJARYSc3VwcG9y
+dEBjYWNlcnQub3JnMB4XDTIxMDQxOTEyMTgzMFoXDTMxMDQxNzEyMTgzMFowVDEU
+MBIGA1UEChMLQ0FjZXJ0IEluYy4xHjAcBgNVBAsTFWh0dHA6Ly93d3cuQ0FjZXJ0
+Lm9yZzEcMBoGA1UEAxMTQ0FjZXJ0IENsYXNzIDMgUm9vdDCCAiIwDQYJKoZIhvcN
+AQEBBQADggIPADCCAgoCggIBAKtJNRFIfNImflOUz0Op3SjXQiqL84d4GVh8D57a
+iX3h++tykA10oZZkq5+gJJlz2uJVdscXe/UErEa4w75/ZI0QbCTzYZzA8pD6Ueb1
+aQFjww9W4kpCz+JEjCUoqMV5CX1GuYrz6fM0KQhF5Byfy5QEHIGoFLOYZcRD7E6C
+jQnRvapbjZLQ7N6QxX8KwuPr5jFaXnQ+lzNZ6MMDPWAzv/fRb0fEze5ig1JuLgia
+pNkVGJGmhZJHsK5I6223IeyFGmhyNav/8BBdwPSUp2rVO5J+TJAFfpPBLIukjmJ0
+FXFuC3ED6q8VOJrU0gVyb4z5K+taciX5OUbjchs+BMNkJyIQKopPWKcDrb60LhPt
+XapI19V91Cp7XPpGBFDkzA5CW4zt2/LP/JaT4NsRNlRiNDiPDGCbO5dWOK3z0luL
+oFvqTpa4fNfVoIZwQNORKbeiPK31jLvPGpKK5DR7wNhsX+kKwsOnIJpa3yxdUly6
+R9Wb7yQocDggL9V/KcCyQQNokszgnMyXS0XvOhAKq3A6mJVwrTWx6oUrpByAITGp
+rmB6gCZIALgBwJNjVSKRPFbnr9s6JfOPMVTqJouBWfmh0VMRxXudA/Z0EeBtsSw/
+LIaRmXGapneLNGDRFLQsrJ2vjBDTn8Rq+G8T/HNZ92ZCdB6K4/jc0m+YnMtHmJVA
+BfvpAgMBAAGjgfIwge8wDwYDVR0TAQH/BAUwAwEB/zBhBggrBgEFBQcBAQRVMFMw
+IwYIKwYBBQUHMAGGF2h0dHA6Ly9vY3NwLkNBY2VydC5vcmcvMCwGCCsGAQUFBzAC
+hiBodHRwOi8vd3d3LkNBY2VydC5vcmcvY2xhc3MzLmNydDBFBgNVHSAEPjA8MDoG
+CysGAQQBgZBKAgMBMCswKQYIKwYBBQUHAgEWHWh0dHA6Ly93d3cuQ0FjZXJ0Lm9y
+Zy9jcHMucGhwMDIGA1UdHwQrMCkwJ6AloCOGIWh0dHBzOi8vd3d3LmNhY2VydC5v
+cmcvY2xhc3MzLmNybDANBgkqhkiG9w0BAQ0FAAOCAgEAxh6td1y0KJvRyI1EEsC9
+dnYEgyEH+BGCf2vBlULAOBG1JXCNiwzB1Wz9HBoDfIv4BjGlnd5BKdSLm4TXPcE3
+hnGjH1thKR5dd3278K25FRkTFOY1gP+mGbQ3hZRB6IjDX+CyBqS7+ECpHTms7eo/
+mARN+Yz5R3lzUvXs3zSX+z534NzRg4i6iHNHWqakFcQNcA0PnksTB37vGD75pQGq
+eSmx51L6UzrIpn+274mhsaFNL85jhX+lKuk71MGjzwoThbuZ15xmkITnZtRQs6Hh
+LSIqJWjDILIrxLqYHehK71xYwrRNhFb3TrsWaEJskrhveM0Os/vvoLNkh/L3iEQ5
+/LnmLMCYJNRALF7I7gsduAJNJrgKGMYvHkt1bo8uIXO8wgNV7qoU4JoaB1ML30QU
+qGcFr0TI06FFdgK2fwy5hulPxm6wuxW0v+iAtXYx/mRkwQpYbcVQtrIDvx1CT1k5
+0cQxi+jIKjkcFWHw3kBoDnCos0/ukegPT7aQnk2AbL4c7nCkuAcEKw1BAlSETkfq
+i5btdlhh58MhewZv1LcL5zQyg8w1puclT3wXQvy8VwPGn0J/mGD4gLLZ9rGcHDUE
+CokxFoWk+u5MCcVqmGbsyG4q5suS3CNslsHURfM8bQK4oLvHR8LCHEBMRcdFBn87
+cSvOK6eB1kdGKLA8ymXxZp8=
+-----END CERTIFICATE-----
+"
+
+certValidAndUIDmatch() {
+    for UID in "${GPG_UID[@]}"; do
+        GPG_MAIL="$(awk -F '[<>]' '{print $2}' <<<"${UID}")"
+        GPG_NAME="$(awk 'NF{--NF};1' <<<"${UID}")"
+
+        if openssl smime -CAfile "${CLASS3_ROOT_CRT}" -verify -verify_email "${GPG_MAIL}" -verify_hostname "${GPG_NAME}" -in "$1" >/dev/null 2>&1; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
+if [ ! -f "$1" ]; then
+    echo -e "\nNo file provided, e.g. \"bash ${0##*/} pubkey.asc.msg\". Aborting...\n"
+else
+    CRT="$(openssl smime -pk7out -in "$1" | openssl pkcs7 -print_certs)"
+    CRL_URI="$(echo "${CRT}" | openssl x509 -noout -ext crlDistributionPoints | grep -Po 'URI:\K.*')"
+    # suite list created with:
+    # openssl ciphers -v -s | grep AEAD | grep ECDHE | awk '{print $1}' | paste -d: -s -
+    #
+    # in case of tlsv1.3, currently unsupported by cacert.org, we take everything
+    CIPHERS="ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256"
+    CRL="$(curl -fsS --ciphers "${CIPHERS}" --proto '=https' --tlsv1.2 "${CRL_URI/http:\/\//https:\/\/}" | openssl crl -inform DER -outform PEM)"
+    IFS=$'\n' read -d '' -r -a GPG_UID < <(gpg --with-colons --show-keys "$1" | grep "^uid" | cut -d: -f10)
+
+    if ! openssl x509 -noout -checkend 0 -in <(echo "${CRT}") >/dev/null 2>&1; then
+        echo -e "\nCertificate expired. Aborting...\n"
+    elif ! openssl ocsp -CAfile <(echo "${CLASS1_ROOT_CRT}") -issuer <(echo "${CLASS3_ROOT_CRT}") -cert <(echo "${CRT}") -url <(echo "${CRT}" | openssl x509 -noout -ocsp_uri) >/dev/null 2>&1 && \
+         ! openssl verify -crl_check -CAfile <(echo -e "${CLASS3_ROOT_CRT}\n${CLASS1_ROOT_CRT}\n${CRL}") <(echo "${CRT}"); then
+        echo -e "\nCRL and OCSP check failed. Aborting...\n"
+    elif ! certValidAndUIDmatch; then
+        echo -e "\nNo valid S/MIME certificate OR no match between GnuPG UIDs and certificate CommonName/mail. Aborting...\n"
+    else
+        cat <<EOF
+
+Checks passed 🎉
+
+GnuPG UIDs:
+$(printf '  - %s\n' "${GPG_UID[@]}")
+
+S/MIME certificate subject:
+  - CommonName: $(echo "${CRT}" | openssl x509 -noout -subject -nameopt esc_ctrl,esc_msb,sep_multiline,lname | grep -Po 'commonName=\K.*')
+  - E-MAIL:     $(echo "${CRT}" | openssl x509 -noout -subject -nameopt esc_ctrl,esc_msb,sep_multiline,lname | grep -Po 'emailAddress=\K.*')
+
+Feel free to import with:
+  gpg --import "$1"
+
+EOF
+    fi
+fi
+
+exit 1
