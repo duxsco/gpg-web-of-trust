6a7be8aaf006a96037e055ef4ac7fcf35b4ec097
David Sardari <d@duxsco.de>
2021-10-10 23:55:09 +0200

    CRL: Take redirection https://www.cacert.org/class3.crl -> http://crl.cacert.org/class3-revoke.crl into consideration

--- s2g.sh
+++ s2g.sh
@@ -92,9 +92,9 @@ function getCRL() {
 
     echo -e "${CLASS3_ROOT_CRT}\n${CLASS1_ROOT_CRT}" | \
     curl \
-        --fail --silent --show-error \
+        --fail --silent --show-error --location \
         --cacert /dev/stdin \
-        --cert-status --proto '=https' --tlsv1.2 \
+        --cert-status --tlsv1.2 \
         --ciphers "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256" \
         "$(openssl x509 -noout -ext crlDistributionPoints <<<"$1" | grep -Po 'URI:\K.*' | sed 's#http://#https://#')" | \
     openssl crl -inform DER -outform PEM
